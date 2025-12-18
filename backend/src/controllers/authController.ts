571ab1dfa943d9e5b8017960dfd0a2861ddd1262
Andres Verdenelli <andresverdenelli@icloud.com>
2025-12-18 15:07:11 -0300

    Add authentication controller with signup, login, and user retrieval endpoints. Refactor auth routes to utilize controller methods for improved code organization and maintainability.

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,82 @@
+import type { RequestHandler } from 'express'
+import { createUser, selectUserByEmail } from '../db/queries/users.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { ENV } from '../config/env.js'
+import type { Payload } from '../types/auth.js'
+
+export const signup: RequestHandler = async (req, res) => {
+	const { email, password } = req.body
+
+	if (!email || !password) {
+		return res.status(400).json({ error: 'email and password required' })
+	}
+
+	try {
+		const existingUser = await selectUserByEmail(email)
+
+		if (existingUser) {
+			return res.status(409).json({ error: 'User already exists' })
+		}
+
+		const hashedPassword = await bcrypt.hash(password, 10)
+
+		const createdUser = await createUser(email, hashedPassword)
+
+		if (!createdUser) {
+			return res.status(400).json({ error: 'User not created' })
+		}
+
+		console.log('✓ Inserted user:', createdUser)
+		return res
+			.status(201)
+			.json({ message: 'User created', email: createdUser.email })
+	} catch (error) {
+		console.error('x DB Insert Error: ', error)
+
+		return res.status(500).json({ error: 'Error creating user' })
+	}
+}
+
+export const login: RequestHandler = async (req, res) => {
+	const { email, password } = req.body
+	if (!email || !password) {
+		return res.status(400).json({ error: 'email and password required' })
+	}
+
+	try {
+		const user = await selectUserByEmail(email)
+
+		if (!user) {
+			return res.status(404).json({ error: 'Invalid Email' })
+		}
+
+		const isPasswordValid = await bcrypt.compare(password, user.password)
+
+		if (!isPasswordValid) {
+			return res.status(401).json({ error: 'Invalid Password' })
+		}
+
+		const payload: Payload = {
+			id: user.id,
+			email: user.email,
+		}
+
+		const token = jwt.sign(payload, ENV.JWT_SECRET, {
+			expiresIn: '15m',
+		})
+
+		return res.status(200).json({
+			message: 'Login successful',
+			token,
+		})
+	} catch (error) {
+		console.error('Login Error', error)
+		return res.status(500).json({ error: 'Database error' })
+	}
+}
+
+export const me: RequestHandler = (req, res) => {
+	const user = req.user
+	return res.json({ user })
+}
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,102 +1,11 @@
 import { Router } from 'express'
-import { createUser, selectUserByEmail } from '../db/queries/users.js'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import { ENV } from '../config/env.js'
-import { users } from '../db/schema.js'
-import { db } from '../db/index.js'
 import { requireAuth } from '../middleware/requireAuth.js'
+import { login, me, signup } from '../controllers/authController.js'
 
 export const authRouter: Router = Router()
 
-authRouter.post('/signup', async (req, res) => {
-	const { email, password } = req.body
+authRouter.post('/signup', signup)
 
-	if (!email || !password) {
-		return res.status(400).json({ error: 'email and password required' })
-	}
+authRouter.post('/login', login)
 
-	try {
-		const existingUser = await selectUserByEmail(email)
-
-		if (existingUser) {
-			return res.status(409).json({ error: 'User already exists' })
-		}
-
-		const hashedPassword = await bcrypt.hash(password, 10)
-
-		const createdUser = await createUser(email, hashedPassword)
-
-		if (!createdUser) {
-			return res.status(400).json({ error: 'User not created' })
-		}
-
-		console.log('✓ Inserted user:', createdUser)
-		return res
-			.status(201)
-			.json({ message: 'User created', email: createdUser.email })
-	} catch (error) {
-		console.error('x DB Insert Error: ', error)
-
-		return res.status(500).json({ error: 'Error creating user' })
-	}
-})
-
-authRouter.post('/login', async (req, res) => {
-	const { email, password } = req.body
-	if (!email || !password) {
-		return res.status(400).json({ error: 'email and password required' })
-	}
-
-	try {
-		const user = await selectUserByEmail(email)
-
-		if (!user) {
-			return res.status(404).json({ error: 'Invalid Email' })
-		}
-
-		const isPasswordValid = await bcrypt.compare(password, user.password)
-
-		if (!isPasswordValid) {
-			return res.status(401).json({ error: 'Invalid Password' })
-		}
-
-		const token = jwt.sign(
-			{
-				id: user.id,
-				email: user.email,
-			},
-			ENV.JWT_SECRET,
-			{
-				expiresIn: '15m',
-			},
-		)
-
-		return res.status(200).json({
-			message: 'Login successful',
-			token,
-		})
-	} catch (error) {
-		console.error('Login Error', error)
-		return res.status(500).json({ error: 'Database error' })
-	}
-})
-
-authRouter.delete('/deleteUsers', async (_req, res) => {
-	try {
-		const deleted = await db.delete(users).returning()
-
-		return res
-			.status(200)
-			.json({ message: `Deleted ${deleted.length} users. Accounts ${deleted}` })
-	} catch (error) {
-		console.error('Delete error: ', error)
-		return res.status(500).json({ error: 'Database error' })
-	}
-})
-
-authRouter.get('/me', requireAuth, (req, res) => {
-	//@ts-expect-error user podria no existir
-	const user = req.user
-	return res.json({ user })
-})
+authRouter.get('/me', requireAuth, me)
